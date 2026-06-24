209cf852d22615299f095cec2825bd97c04b5cc2
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-06-24 16:27:38 +0530

    Time: 25 ms (12.37%), Space: 22.1 MB (90.73%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
     int maxDistance(string moves) {
-       int uc=0;
-       int negc=0;
-       int plusc=0;
-       pair<int,int> point = {0,0};
-
+        int x=0;
+        int y=0;
+        int u=0;
        for(auto m : moves){
             if( m == 'L'){
-                negc+=1;
-                point.first -= 1;
+                x--;
             }
             else if( m=='D'){
-                negc+=1;
-                point.second -= 1;
+                y--;
             }
             else if( m=='R'){
-                plusc+=1;
-                point.first += 1;
+                x++;
             }
-            else if( m=='U'){
-                plusc+=1;
-                point.second += 1;
+            else if( m =='U'){
+                y++;
             }
             else 
-                uc+=1;
+                u += 1;
         } 
-
-        int curSum = point.first + point.second;
-        if(curSum >= 0){
-            if(point.first >=0 ) 
-                point.first += uc;
-            else 
-                point.second+=uc;
-        }
-        else {
-            if(point.first >=0 ) 
-                point.second -= uc;
-            else 
-                point.first -= uc;
-        }
-        int ans = abs(0-point.first) + abs(0-point.second);
-
-        return ans;
+        return abs(x) + abs(y) + u;;
     }
 };
