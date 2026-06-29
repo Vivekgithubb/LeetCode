9b5ab5136c7bee407fb8fa1f56d8fc1268994e09
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-06-29 12:16:19 +0530

    Time: 6 ms (77.87%), Space: 57.3 MB (4.48%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -3,18 +3,31 @@ public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         int n = arr.size();
         //sort
-        sort(arr.begin(),arr.end());
+        // sort(arr.begin(),arr.end());
 
-        //set first element to 1
-        arr[0] = 1;
+        // //set first element to 1
+        // arr[0] = 1;
 
-        //change all elements so the abs diff is 1 
-        for(int i=0;i<n-1;i++){
-            if(arr[i+1] > arr[i]+1)
-                arr[i+1] = arr[i]+1;
+        // //change all elements so the abs diff is 1 
+        // for(int i=0;i<n-1;i++){
+        //     if(arr[i+1] > arr[i]+1)
+        //         arr[i+1] = arr[i]+1;
+        // }
+
+        // //last element is max
+        // return arr[n-1];
+
+        vector<int>freq(n+1,0);
+        for(int i=0;i<n;i++){
+            freq[min(arr[i],n)]++;
         }
 
-        //last element is max
-        return arr[n-1];
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            if(freq[i] == 0) continue;
+            ans = min(ans+freq[i] , i);
+        }
+
+        return ans;
     }
 };
